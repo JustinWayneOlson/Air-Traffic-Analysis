58d91388724af420bf30726a1fef09c1a30e133a
Justin <justinwayneolson@gmail.com>
2016-10-06 11:44:39 -0600

    added basic db_init.py script as a template

---
+++ data/db_init.py
@@ -0,0 +1,28 @@
+import psycopg2
+
+USER='postgres'
+DBNAME='testing'
+PASSWORD='postgres'
+print "Opening Database Connection"
+con = psycopg2.connect('host=localhost dbname={} user={} password={}'.format(DBNAME, USER, PASSWORD))
+
+cursor = con.cursor()
+print("Connected")
+
+try:
+    cursor.execute("DROP TABLE test_table CASCADE")
+    con.commit()
+except:
+    pass
+
+cursor.execute("CREATE TABLE test_table ( \
+                    id      SERIAL PRIMARY KEY,\
+                    name    TEXT,\
+                    age     INTEGER\
+                    );")
+con.commit()
+con.close()
+
+
+
+
