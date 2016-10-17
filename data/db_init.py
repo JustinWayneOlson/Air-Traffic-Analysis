185cae9f1a58dfe7fbac20da490ec8211434fa1c
Matt Oakley <Matt Oakley>
2016-10-17 17:40:52 -0600

    SQL Create, Update, and Insert scripts

--- data/db_init.py
+++ data/db_init.py
@@ -1,25 +1,21 @@
 import psycopg2
 
-USER='postgres'
-DBNAME='testing'
-PASSWORD='postgres'
+USER='test'
+DBNAME='asdf'
+PASSWORD='pass'
 print "Opening Database Connection"
 con = psycopg2.connect('host=localhost dbname={} user={} password={}'.format(DBNAME, USER, PASSWORD))
 
 cursor = con.cursor()
 print("Connected")
 
-try:
-    cursor.execute("DROP TABLE test_table CASCADE")
-    con.commit()
-except:
-    pass
+# #try:
+# #    cursor.execute("DROP TABLE test_table CASCADE")
+#     con.commit()
+# except:
+#     pass
 
-cursor.execute("CREATE TABLE test_table ( \
-                    id      SERIAL PRIMARY KEY,\
-                    name    TEXT,\
-                    age     INTEGER\
-                    );")
+cursor.execute("CREATE TABLE test_table ( id      SERIAL PRIMARY KEY, name    TEXT,age     INTEGER);")
 con.commit()
 con.close()
 
