71e215af318bdf29aa949e3166088fe13dad3060
mitchtz <mitchtz@gmail.com>
2016-10-17 18:02:26 -0600

    Minor fixes to tornado app.py file, sreves index correctly, small clarifications

--- application/src/app.py
+++ application/src/app.py
@@ -8,28 +8,28 @@ import random
 #Serve index.html
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
+        #Patht to the webpage to be served
         self.render("./html/index.html")
-
-#Server hello world
-class HelloWorldHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
 
 #Get request takes in one arg via url and sends data back
 class TestGetHandler(tornado.web.RequestHandler):
     def get(self, data):
+        print("Serving JSON response to:" + data)
+        #Convert passed in get request into int
         data=int(data)
-        return_data={
-                'response':[]
-        }
+        #Create dict to store data to return
+        return_data={'response':[]}
+        #Create random data
+        '''Get data from database at this point'''
         for i in range(0,data):
             return_data['response'].append(random.random() * 10)
+        #Write dict content to page, like a json response
         self.write(return_data)
 
 #POST request takes in JSON from body, and returns JSON
 class TestPostHandler(tornado.web.RequestHandler):
     def post(self):
-        print json_decode(self.request.body)
+        print(json_decode(self.request.body))
         return_data={
                 'response':{
                     'message':'It Worked!!',
@@ -43,7 +43,6 @@ class TestPostHandler(tornado.web.RequestHandler):
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-        (r"/hello", HelloWorldHandler),
         #localhost:8888/testget/(any single digit number)
         (r"/testget/([0-9]*)", TestGetHandler),
         (r"/testpost", TestPostHandler)
