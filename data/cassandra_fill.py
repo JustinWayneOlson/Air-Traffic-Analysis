45b1ff5f60312e482564b887f3043fd71d2232ed
Justin Olson <justinwayneolson@gmail.com>
2017-04-02 16:27:32 -0400

    fixed datatypes in table added flag to stop reading .cql file

--- data/cassandra_fill.py
+++ data/cassandra_fill.py
@@ -7,12 +7,16 @@ import collections
 
 def main(argv):
    field_pattern = re.compile('\"([^\"]*)\"\s([^,]*),')
+   end_pattern = re.compile('\/\*End Transtats Table\*\/')
    columns = []
    datatypes = []
    with open('cassandrainit.cql') as init_file:
       for line in init_file:
          match = re.match(field_pattern, line)
-         if not(match):
+         end_match = re.match(end_pattern, line)
+         if (end_match):
+            break
+         elif not(match):
             continue
          else:
             columns.append(match.group(1))
@@ -26,6 +30,7 @@ def main(argv):
    with open(csv_file) as csvfile:
       csvdata = csv.reader(csvfile)
       for insert_column in csvdata:
+         print len(insert_column)
          if(first):
             first = False
             continue
@@ -42,6 +47,8 @@ def main(argv):
                value_string += "'{}',".format(value)
          start_string = "INSERT INTO AirportTrafficAnalytics.Transtats({}) VALUES ({});".format(column_labels, value_string[:-1])
          session.execute("USE AirportTrafficAnalytics")
+         start_string = start_string.replace('\n', '')
+         print start_string
          session.execute(start_string)
 if __name__ == "__main__":
    main(sys.argv)
