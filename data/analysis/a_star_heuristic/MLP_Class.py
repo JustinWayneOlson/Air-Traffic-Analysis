a2740560703c2ee1d2f08b152166466eb20e3ec3
mitchtz <mitchtz@gmail.com>
2017-04-02 23:30:11 -0600

    Reading from database works, just needs data to pull

--- data/analysis/a_star_heuristic/MLP_Class.py
+++ data/analysis/a_star_heuristic/MLP_Class.py
@@ -78,20 +78,18 @@ class MLP:
 
 	#Read data in from the cassandra database. Returns the number of data points that could be successfully read
 	def read_database(self):
+		#Connect to cassandra cluster
 		cluster = Cluster(["localhost"])
 		session = cluster.connect()
 		session.row_factory = dict_factory
 		session.execute("USE AirportTrafficAnalytics")
-		query = "SELECT * from transtats WHERE LIMIT %s" % (self.rows)
+		query = """SELECT * from transtats WHERE "Year"=%s AND "Month"=%s LIMIT %s ALLOW FILTERING""" % (self.year, self.month, self.rows)
 		response = session.execute(query)
 
-		#List of the indices of the columns to use as features
-		feature_list = ["Month", "DayOfWeek", "CRSDepTime", "Distance"]#[2,4,31,56]#[2,4,11,21,31,56] #Month(2), Day of Week(4), Listed Departure Time(31) #10,31 late additions
-		#Index of the column to use as a label. 34 for Departure Delay, 45 for Arrival Delay
-		label = "AirTime"#54 #53 for total time of the flight, 54 for only time in the air
 
 		failed = 0
 		for i in response:
+			#print(float(i[feat]) for feat in self.features)
 			'''
 			x.append([i[feat] for feat in feature_list])
 			y.append(i[label])
@@ -104,14 +102,16 @@ class MLP:
 				else:
 					y_temp = 0
 				#Add items to features list based on indicies given
-				x_temp = [float(i[feat]) for feat in self.feature_list]
+				x_temp = [float(i[feat]) for feat in self.features]
 				
 				self.y.append(y_temp)
 				#x.append([float(info[2].replace("\"", "")), float(info[4].replace("\"", "")),float(info[36].replace("\"", ""))])
 				self.x.append(x_temp)
 
 			except:
-			failed += 1
+				failed += 1
+		#Return the number of data points that could be read successfully
+		return len(self.x)
 
 	#Train the model and record it's accuracy. Checks if data has been read in before training. Returns True if training succeeded, False if training failed.
 	def train(self):
@@ -128,12 +128,23 @@ class MLP:
 
 	#Persist the current trained model to the database. Checks if the model has been created yet, and that accuracy is above 0% and has been trained on a data set > 0
 	def save(self):
-		pass
-		#TODO
+		#Check if the model has been trained on more than 0 inputs, and accuracy is above 0
+		if ((len(self.x) > 0) and (self.accuracy != 0)):
+			#Connect to cassandra cluster
+			cluster = Cluster(["localhost"])
+			session = cluster.connect()
+			session.row_factory = dict_factory
+			session.execute("USE AirportTrafficAnalytics")
+			query = """INSERT INTO transtats ("Year", "Month", "Round")""" % (self.rows)
+			response = session.execute(query)
+		#Otherwise return false
+		else:
+			return False
 
 	#Checks the database to see if there is already a trained model with the same parameters as the current model. Returns True if one exists, Flase otherwise
 	def check_database(self):
 		pass
+		#SELECT primary_keys FROM TABLE WHERE primary_keys = blah LIMIT 1; Just try to query for the record, and if there is a result return True
 		#TODO
 
 	#Getter that returns the % accuracy of the currently trained model.
@@ -146,31 +157,36 @@ class MLP:
 if __name__ == "__main__":
 	import time
 	start_time = time.time()
-	'''IMPORTANT VARIABLES'''
 	#Number of rows to read in from csv files
-	row_num = 200000
+	row_num = 2#00000
 	#Configurations that have worked well generally. (0.005 | 500,100), (0.1 | 500,100), (0.005 | 2000), (0.01 | 500), (0.01 | 2000)
 	#Configurations to pickle. Format is [(year, month, alpha, h layer)]
-	y = 2015
-	m = 9
+	y = 2016
+	m = 7
 	alpha = 0.1
 	layer = (500,100)
 	#Granulatity to round to
 	round_base = 20 #Will be off by round_base/2 at most. Ex. With round_base=20, 11 rounds to 20, 10 rounds to 0
 	#List of the indices of the columns to use as features
-	feature_list = [2,4,31,56]#[2,4,11,21,31,56] #Month(2), Day of Week(4), Listed Departure Time(31) #10,31 late additions
+	feature_list = ["Month", "DayOfWeek", "CRSDepTime", "Distance"]#[2,4,31,56]#[2,4,11,21,31,56] #Month(2), Day of Week(4), Listed Departure Time(31) #10,31 late additions
 	#Index of the column to use as a label. 34 for Departure Delay, 45 for Arrival Delay
-	label = 54 #53 for total time of the flight, 54 for only time in the air
+	label = "AirTime"#54 #53 for total time of the flight, 54 for only time in the air
+	
 	#Create object
 	test_mlp = MLP(y,m,row_num,feature_list,label,round_base,alpha,layer)
 	#Read in data
-	print("Reading in data from csv")
-	print(test_mlp.read_csv())
+	#print("Reading in data from csv")
+	#print(test_mlp.read_csv())
+	print("Reading in data from Cassandra")
+	print(test_mlp.read_database())
 	#Train classifier
-	print("Training")
-	print(test_mlp.train())
-	print("Accurcy")
-	print(test_mlp.get_accuracy())
+	##print("Training")
+	##print(test_mlp.train())
+	##print("Accuracy")
+	##print(test_mlp.get_accuracy())
+	print(test_mlp.x)
 
 	prog_time = time.time()-start_time
 	print("Ran in", prog_time, "seconds")
+
+
