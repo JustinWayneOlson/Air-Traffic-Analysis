5856fdc88062db7956756a56f21364498187681b
mmuehlbradt <micha.muehlbradt@gmail.com>
2017-04-20 11:54:37 -0600

    Added query to read from Routing table

--- routing/routingFrontEnd.py
+++ routing/routingFrontEnd.py
@@ -5,15 +5,14 @@ from cassandra.query import dict_factory
 def readDatabase(jobName):
 
 	job = str(jobName)
-	rows = None
 	#connect to Cassandra cluster
 	cluster = Cluster(["localhost"])
 	session = cluster.connect()
 	session.row_factory = dict_factory
 	session.execute("USE AirportTrafficAnalytics")
-	query = """SELECT * from Routing WHERE "jobName"=%s """ % (job)
-	response = session.execute(query)
-	
+	query = """SELECT * from Routing WHERE "jobName"= '%s' """ % (job)
+	rows = session.execute(query)
+	response = rows[0]	
 	print(response)
 
 readDatabase("Route01")
